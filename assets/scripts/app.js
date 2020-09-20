68295380da486343434573f5179a498e995c47f5
Erica VanNorman <66809260+evannor@users.noreply.github.com>
2020-09-20 15:53:13 -0700

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,48 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput() {
+  // Will need to get this number in multiple functions, leaving it outside will make the code simpler to follow and adjust
+  return parseFloat(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  // place at top of function so that currentResult will output the previous value, and not the one adjusted after the function was called
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  // +userInput.value will also change the string to a number
+  // intvalue.toString() will change the integer to a string
+  createAndWriteOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
